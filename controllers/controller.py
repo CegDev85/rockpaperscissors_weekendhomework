2c06d37e9dd0766d3239ef6815aed16be4772843
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-05-22 23:10:21 +0100

    adding name save

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,7 +6,17 @@ from modules import player
 @app.route('/rps')
 def index():
     return render_template('base.html')
-    
+
+@app.route('/rps/play')
+def name_entry():
+    return render_template('new_game.html')
+
+@app.route('/rps/play/rules')
+def game_rules():
+    return render_template('how-to-play.html')
+
+@app.route()
+
 
 @app.route('/rps/play/<choice_1>/<choice_2>')
 def play(choice_1,choice_2):
